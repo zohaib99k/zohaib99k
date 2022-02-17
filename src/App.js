530ba88445e4c1976c2dcd21d5c654b3eb60167d
zohaib99k <gg98190@gmail.com>
2022-02-17 17:43:50 +0500

    Add:header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'; 
 import './App.css';
 import Header from './Components/Header';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
     </div>
   );
 }
